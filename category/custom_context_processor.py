2fc393250202e6f107947b396d733416fa168ae6
Subash Niroula <subash.niroula4455@gmail.com>
2021-07-16 08:42:30 +0545

    context processor code optimized

--- category/custom_context_processor.py
+++ category/custom_context_processor.py
@@ -3,11 +3,8 @@ from django.db.models import Count
 
 def category_renderer(request):
     all = Category.objects.annotate(num_post=Count('post')).order_by('-num_post')
-    _top_categories = []
     max_range = all.count() if all.count()<5 else 5
-    for c in range(0,max_range,1):
-        _top_categories.append(all[c])
-
+    _top_categories = all[:max_range]
     return {
        'all_categories': Category.objects.all(),
        'top_categories': _top_categories,
